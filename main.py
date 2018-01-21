f8ddc1392e7cbea027b77c4b0c282802d8f5f3d1
mavx <auyongminhao@gmail.com>
2018-01-21 13:42:47 +0800

    Add slack notification when balance is found

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import requests
 import keys
 
 ENDPOINT = 'https://blockchain.info/balance'
+SLACK_WEBHOOK = 'https://hooks.slack.com/services/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'
 
 def main():
     # Initialize address generator
@@ -38,6 +39,11 @@ def address_search(key_generator_instance, db_connection, batch_size):
                 private_key=address_dict[address],
                 balance=balances[address]
             )
+            send_address_details(
+                public_key=address, 
+                private_key=address_dict[address_dict],
+                balance=balances[address]
+            )
 
 
 def generate_address_dict(key_generator_instance, batch_size):
@@ -47,6 +53,7 @@ def generate_address_dict(key_generator_instance, batch_size):
         address_dict[keypair['public_key']] = keypair['private_key']
     
     return address_dict
+
 
 def get_balances(address_list):
     print('Querying addresses for balance...')
@@ -76,6 +83,23 @@ def save_address(connection, public_key, private_key, balance):
     connection.commit()
     c.close()
 
+
+def send_address_details(public_key, private_key, balance):
+    message = (
+        "Public Key: {0}\n"
+        "Private Key: {1}\n"
+        "Balance: {2}"
+    ).format(public_key, private_key, balance)
+    payload = {
+        'attachments': [{
+            'fallback': message,
+            'fields': [{
+                'title': 'BTC Address',
+                'value': message
+            }]
+        }]
+    }
+    return requests.post(SLACK_WEBHOOK, json=payload)
 
 if __name__ == '__main__':
     main()
